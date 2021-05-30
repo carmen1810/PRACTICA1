bcfd3adaa1921558f911a077c7d29bec76c78152
carmen1810 <84483934+carmen1810@users.noreply.github.com>
2021-05-30 15:19:30 -0500

    Update practica1.js

--- practica1.js
+++ practica1.js
@@ -48,10 +48,22 @@ console.log('user',user)
 delete user.surname
 console.log('user',user)
 ///ejercicio 6//
+
 let name={}
 alert(isEmpty(name));//true
 schedule["8:30"] = "get up";
 alert(isEmpty(schedule) ); // false
+let name={}
+
+function isEmpty(obj) {
+   for ( var key in obj) {
+  	     return false ; 
+     }
+  	   
+     return true ;
+  }
+  isEmpty(name)
+
 ///Ejercicio 7///
 
 let salaries = {
