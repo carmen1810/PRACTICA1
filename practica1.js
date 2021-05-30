014bb2344d58c58a1c415a137d3e4fdf5738f51b
carmen1810 <84483934+carmen1810@users.noreply.github.com>
2021-05-30 11:19:55 -0500

    Update practica1.js

--- practica1.js
+++ practica1.js
@@ -1,3 +1,47 @@
-let admin
+
+///Ejercicio 1//
+
 let name
-console.log()
+let admin
+name="John";
+admin=name
+alert(admin)
+console.log('name',name)
+console.log('admin',admin)
+
+
+
+///Ejercicio 2//
+
+let planeta = prompt("What is the name of our planet?")
+alert('Hello, our planet is '+ planeta + '!')
+
+let visitorwebsite="visit"
+
+///Ejercicio 3//
+
+const birthday='18.04.1984'
+const age=someCode(birthay)
+//Se debe empezar con mayuscula y seguido cn minuscula para que sea entendible
+///
+
+
+///Ejercicio 4//
+
+let name ="Ilya";
+console.log('name', name);
+
+alert('hello $ {1}')// se muestra solo 'hello 1'
+alert('hello $ {"name"}')//se muestra solo 'hello name'
+alert('hello $ {name}')//se muestra 'hello name Ilya'
+
+///Ejercicio 5///
+
+const objectConstructor= new Object();
+const user={ name: 'John',
+surname: 'Smith'}
+console.log('user',user)
+
+ejercicio 6
+ejercicio 7
+ejercicio 8
