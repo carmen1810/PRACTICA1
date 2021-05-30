479ad58d1ebeaa5e24c77eb390d673029e5e1b7f
carmen1810 <84483934+carmen1810@users.noreply.github.com>
2021-05-30 15:06:13 -0500

    Update practica1.js

--- practica1.js
+++ practica1.js
@@ -76,8 +76,19 @@ let salaries = {
 
 
 //Ejercicio 8///
-let menu = {
-  width: 200,
-  height: 300,
-  title: "My menu"
-  };
+ let menu = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+    };
+  
+  console.log('menu',menu)
+  function multiplyNumeric(obj) {
+    for (let key in obj) {
+      if (typeof obj[key] == 'number') {
+        obj[key] *= 2;
+      }
+    }
+  }
+  
+  console.log(multiplyNumeric(menu))
